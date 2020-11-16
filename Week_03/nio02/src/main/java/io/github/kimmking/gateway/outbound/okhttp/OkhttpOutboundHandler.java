456ae9609265ec90e740722ae211a5df847aa77d
weiyinjie <408967878@qq.com>
2020-11-16 15:33:47 +0800

    添加线程池

--- Week_03/nio02/src/main/java/io/github/kimmking/gateway/outbound/okhttp/OkhttpOutboundHandler.java
+++ Week_03/nio02/src/main/java/io/github/kimmking/gateway/outbound/okhttp/OkhttpOutboundHandler.java
@@ -10,11 +10,15 @@ import io.netty.handler.codec.http.FullHttpRequest;
 import io.netty.handler.codec.http.FullHttpResponse;
 import io.netty.handler.codec.http.HttpUtil;
 import okhttp3.*;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import javax.security.auth.login.AccountLockedException;
 import java.io.IOException;
+import java.io.UnsupportedEncodingException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.*;
 
 import static io.netty.handler.codec.http.HttpHeaderNames.CONNECTION;
 import static io.netty.handler.codec.http.HttpHeaderValues.KEEP_ALIVE;
@@ -25,11 +29,15 @@ import static io.netty.handler.codec.http.HttpVersion.HTTP_1_1;
 
 public class OkhttpOutboundHandler {
 
+    private Logger logger  = LoggerFactory.getLogger(this.getClass());
+
     private OkHttpClient client;
 
     private HttpEndpointRouter httpEndpointRouter;
 
     private List<String> routes;
+
+    private ExecutorService executorService = Executors.newFixedThreadPool(12);
 
     public OkhttpOutboundHandler(List<String> routes) {
         if (routes != null && routes.size() > 0) {
@@ -47,32 +55,38 @@ public class OkhttpOutboundHandler {
     }
 
     public void handle(final FullHttpRequest fullRequest, final ChannelHandlerContext ctx) {
-        // 路由
-        final String url = httpEndpointRouter.route(routes) + fullRequest.uri();
-        // 拼接请求信息
-        Request request = new Request.Builder()
-                .get()
-                .url(url)
-                .build();
-        Call call = client.newCall(request);
-        FullHttpResponse response = null;
-        try {
-            Response res = call.execute();
-            String value = res.body().string();
-            response = new DefaultFullHttpResponse(HTTP_1_1, OK, Unpooled.wrappedBuffer(value.getBytes("UTF-8")));
-            response.headers().set("Content-Type", "application/json");
-            response.headers().setInt("Content-Length", response.content().readableBytes());
-        } catch (IOException e) {
-            e.printStackTrace();
-        } finally {
-            if (fullRequest != null) {
-                if (!HttpUtil.isKeepAlive(fullRequest)) {
-                    ctx.write(response).addListener(ChannelFutureListener.CLOSE);
-                } else {
-                    response.headers().set(CONNECTION, KEEP_ALIVE);
-                    ctx.write(response);
+        executorService.execute(() -> {
+            // 拼装请求地址（路由）
+            final String url = httpEndpointRouter.route(routes) + fullRequest.uri();
+            // 拼接请求信息
+            Request request = new Request.Builder()
+                    .get()
+                    .url(url)
+                    .build();
+            Call call = client.newCall(request);
+            FullHttpResponse response = null;
+            try {
+                Response res = call.execute();
+                String result = res.body().string();
+                logger.info("result is {}", result);
+                response = new DefaultFullHttpResponse(HTTP_1_1, OK, Unpooled.wrappedBuffer(result.getBytes("UTF-8")));
+                response.headers().set("Content-Type", "application/json");
+                response.headers().setInt("Content-Length", response.content().readableBytes());
+            } catch (IOException e) {
+                e.printStackTrace();
+            } finally {
+                if (fullRequest != null) {
+                    if (!HttpUtil.isKeepAlive(fullRequest)) {
+                        ctx.write(response).addListener(ChannelFutureListener.CLOSE);
+                    } else {
+                        response.headers().set(CONNECTION, KEEP_ALIVE);
+                        ctx.write(response);
+                    }
                 }
+                ctx.flush();
             }
-        }
+
+        });
+
     }
 }
